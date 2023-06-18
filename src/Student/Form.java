4a50998db805d74b76e26b5292fcf3597774174d
Trong Linh <linhpksw@gmail.com>
2023-06-18 12:07:02 +0700

    initialize infor ui

--- src/Student/Form.java
+++ src/Student/Form.java
@@ -13,16 +13,14 @@ import ActionField.TableActionEvent;
 import Search.SearchAction;
 
 public class Form extends javax.swing.JFrame {
+//     Creates new form Form
 
-    /**
-     * Creates new form Form
-     */
     private StudentManager mng;
     private DefaultTableModel tableModel;
 
     public Form(StudentManager manager) {
         this.mng = manager;
-        initComponents();       
+        initComponents();
         init();
     }
 
@@ -38,13 +36,14 @@ public class Form extends javax.swing.JFrame {
                 StudentManager.xoaSinhVien(studentID);
                 System.out.println(studentID);
                 System.out.println(mng.getDanhSachSinhVien());
-                updateTableData();                
-            }           
+                updateTableData();
+            }
         };
+
         SearchAction search = new SearchAction() {
             @Override
             public void onSearch() {
-                if(textFieldAnimation1.getText().equals("table")){
+                if (textFieldAnimation1.getText().equals("table")) {
                     updateTableData();
                     return;
                 }
@@ -58,18 +57,18 @@ public class Form extends javax.swing.JFrame {
 //                            "Sex: " + student.getSex());
                     DefaultTableModel model = (DefaultTableModel) studentTable.getModel();
                     model.setRowCount(0); // Clear the existing table data
-            
+
                     Object[] row = {
-                        mng.getDanhSachSinhVien().indexOf(student)+1,
+                        mng.getDanhSachSinhVien().indexOf(student) + 1,
                         student.getFullName(),
                         student.getStudentCode(),
                         student.getSex(),
                         student.getResult(),
                         student.status()
                     };
-                     
+
                     model.addRow(row);
-                    
+
                 } else {
                     JOptionPane.showMessageDialog(null, "Student not found!");
                 }
@@ -78,7 +77,7 @@ public class Form extends javax.swing.JFrame {
             @Override
             public void resetTable() {
                 updateTableData();
-                
+
             }
         };
         textFieldAnimation1.initEvent(search);
@@ -88,22 +87,29 @@ public class Form extends javax.swing.JFrame {
         studentTable.fixTable(jScrollPane2);
 
         studentTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+
             @Override
             public void valueChanged(ListSelectionEvent e) {
                 if (!e.getValueIsAdjusting()) {
                     int selectedRow = studentTable.getSelectedRow();
+
                     if (selectedRow != -1) {
                         String getFullName = studentTable.getValueAt(selectedRow, 1).toString();
                         String getGender = studentTable.getValueAt(selectedRow, 2).toString();
                         String getStudentID = studentTable.getValueAt(selectedRow, 3).toString();
-                        double getTotal = Double.parseDouble(studentTable.getValueAt(selectedRow, 4).toString());
+                        double getTotal = Double.parseDouble(
+                                studentTable.getValueAt(selectedRow, 4).toString());
                         String getStatus = studentTable.getValueAt(selectedRow, 5).toString();
-                        System.out.println("Selected Row Data: " + getFullName + " " + getGender + " " + getStudentID
+
+                        System.out.println("Selected Row Data: " + getFullName + " "
+                                + getGender + " " + getStudentID
                                 + " " + getTotal + " " + getStatus);
+
                     }
                 }
             }
         });
+
         jButton1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -146,7 +152,7 @@ public class Form extends javax.swing.JFrame {
         jScrollPane2 = new javax.swing.JScrollPane();
         studentTable = new Table.Table();
         jButton1 = new javax.swing.JButton();
-        textFieldAnimation1 = new Search.TextFieldAnimation();
+        infor1 = new Detail.Infor();
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -210,31 +216,39 @@ public class Form extends javax.swing.JFrame {
 
         jButton1.setText("jButton1");
 
+        javax.swing.GroupLayout infor1Layout = new javax.swing.GroupLayout(infor1);
+        infor1.setLayout(infor1Layout);
+        infor1Layout.setHorizontalGroup(
+            infor1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 290, Short.MAX_VALUE)
+        );
+        infor1Layout.setVerticalGroup(
+            infor1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 369, Short.MAX_VALUE)
+        );
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                 .addGap(60, 60, 60)
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 81, Short.MAX_VALUE)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addComponent(textFieldAnimation1, javax.swing.GroupLayout.PREFERRED_SIZE, 295, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton1)
-                        .addGap(299, 299, 299))
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(459, Short.MAX_VALUE))))
+                    .addComponent(jButton1)
+                    .addComponent(infor1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(88, 88, 88))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(78, 78, 78)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(textFieldAnimation1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(87, 87, 87)
-                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(84, 84, 84)
+                .addComponent(jButton1)
+                .addGap(92, 92, 92)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(infor1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(71, 71, 71))
         );
 
@@ -256,9 +270,6 @@ public class Form extends javax.swing.JFrame {
 //        AddForm addform = new AddForm(Form.this, mng);
 //        addform.setVisible(true);
 //    }// GEN-LAST:event_jButton2ActionPerformed
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String args[]) {
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -294,12 +305,12 @@ public class Form extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private Detail.Infor infor1;
     private javax.swing.JButton jButton1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane2;
     private Table.Table studentTable;
-    private Search.TextFieldAnimation textFieldAnimation1;
     // End of variables declaration//GEN-END:variables
 }
